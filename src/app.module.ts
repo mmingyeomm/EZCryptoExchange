fe2dda72edd2a7cb3b85308da2b21b5834e42cd5
mmingyeomm <aliveck0208@gmail.com>
2024-07-07 04:04:37 +0900

    rmdb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/user.entity';
+// import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
 import { DataSource } from 'typeorm';
 import { PassportModule } from '@nestjs/passport';
@@ -11,18 +11,20 @@ import { TransactionModule } from './transaction/transaction.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), UserModule , AuthModule, TypeOrmModule.forRoot({
+  imports: [ConfigModule.forRoot({isGlobal: true}), UserModule , AuthModule, 
+    
+  //   TypeOrmModule.forRoot({
 
-    type: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    port: 3306,
-    password: 'root',
-    database: 'ezcryptoexchange',
-    entities: [User],
-    synchronize: true,
+  //   type: 'mysql',
+  //   host: 'localhost',
+  //   username: 'root',
+  //   port: 3306,
+  //   password: 'root',
+  //   database: 'ezcryptoexchange',
+  //   entities: [User],
+  //   synchronize: true,
 
-  }), 
+  // }), 
   PassportModule.register({session:true}), WalletModule, TransactionModule, OrderModule, 
 ],
   controllers: [],
