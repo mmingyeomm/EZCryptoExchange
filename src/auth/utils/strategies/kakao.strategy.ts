ca65b5c194f7946f69b78c001173f744b6ecb294
mmingyeomm <aliveck0208@gmail.com>
2024-07-08 02:41:39 +0900

    changeuri

--- src/auth/utils/strategies/kakao.strategy.ts
+++ src/auth/utils/strategies/kakao.strategy.ts
@@ -15,7 +15,7 @@ export class KakaoStrategy extends PassportStrategy(Strategy) {
       clientID: "ea6e7a064dd68c986c9c28153f2e9d4d",
 
       clientSecret: "XT15CeDinRRx2M9Ju3h70S1AiDpghKjM",
-      callbackURL: "http://localhost:3001/api/auth/kakao/redirect",
+      callbackURL: "http://52.78.206.45:3001/api/auth/kakao/redirect",
     });
   }
 
